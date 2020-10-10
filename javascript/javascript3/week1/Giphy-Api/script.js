26037d8e0e9a6d0c234407b7bb0912392aa966e4
hamedveloper <h.karachi@gmail.com>
2020-10-10 14:24:06 +0200

    Changed promise based on the function

--- javascript/javascript3/week1/Giphy-Api/script.js
+++ javascript/javascript3/week1/Giphy-Api/script.js
@@ -7,18 +7,32 @@ getGifBtn.addEventListener('click', onClickSearch)
 
 getNumber.addEventListener('input', onClickSearch)
 
+const giphyData = async (searchKey, searchNumber) => {
+const API_KEY = "92CWtdMWMOYaLWV6sKMC3WZgM6grBT4l";
+    const data = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchKey}&limit=${searchNumber}&offset=0&rating=g&lang=en`)
+        .then((response) => response.json());
+return data;
+};
+
+
+
+
+
+
 async function onClickSearch() {
 
     pictureBox.innerHTML = '';
 
-    const search = searchKey.value;
+    let search = searchKey.value;
 
-    let image = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=92CWtdMWMOYaLWV6sKMC3WZgM6grBT4l&q=${search}&limit=50&offset=0&rating=g&lang=en`)
-    let imageFinal = await image.json()
+    console.log(giphyData(search, 50))
+    //let image = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=92CWtdMWMOYaLWV6sKMC3WZgM6grBT4l&q=${search}&limit=50&offset=0&rating=g&lang=en`)
+    //let imageFinal = await image.json()
 
-    console.log(imageFinal);
+    //console.log(imageFinal);
 
     for (let i = 0; i < 50; i++) {
+
 
         let ul = document.createElement('ul')
 
@@ -26,7 +40,7 @@ async function onClickSearch() {
 
         let picHolder = document.createElement('img')
 
-        picHolder.src = imageFinal.data[i].images.downsized.url;
+        picHolder.src = giphyData(search, 50).data[i].images.downsized.url;
         ul.appendChild(picHolder)
     }
 }
